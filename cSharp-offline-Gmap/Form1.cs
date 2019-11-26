c05fdc23c5b0349b112a1724d88e9dbb1302fa2c
Fatih Güven <31927632+fatihgvn@users.noreply.github.com>
2019-11-26 21:04:14 +0300

    cache bitti

--- cSharp-offline-Gmap/Form1.cs
+++ cSharp-offline-Gmap/Form1.cs
@@ -9,11 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using GMap.NET;
 using GMap.NET.MapProviders;
+using System.IO;
 
 namespace cSharp_offline_Gmap
 {
     public partial class Form1 : Form
     {
+        string cpath = Directory.GetCurrentDirectory();
+        string cacheFolder = String.Empty;
         public Form1()
         {
             InitializeComponent();
@@ -21,13 +24,25 @@ namespace cSharp_offline_Gmap
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            cacheFolder = Path.Combine(cpath, "Cache");
+
+            if (!Directory.Exists(cacheFolder))
+                Directory.CreateDirectory(cacheFolder);
+            
+            // cache ayarları
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gMapControl1.CacheLocation = cacheFolder;
+
+            // harita kontrolleri
             gMapControl1.DragButton = MouseButtons.Right;
             gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
 
+            // başlnagıç ve harita tipi
             gMapControl1.MapProvider = GMapProviders.GoogleSatelliteMap;
             gMapControl1.Position = new PointLatLng(41.0133045, 29.0630608);
 
-            gMapControl1.MaxZoom = 100;
+            // yakınlaştırma ayarları
+            gMapControl1.MaxZoom = 40;
             gMapControl1.MinZoom = 5;
             gMapControl1.Zoom = 10;
         }
