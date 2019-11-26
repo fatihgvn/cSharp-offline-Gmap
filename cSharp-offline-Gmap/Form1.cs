d76a6ff7c32ba301129f6d7264c7786d72fd60e1
Fatih Güven <31927632+fatihgvn@users.noreply.github.com>
2019-11-26 20:21:23 +0300

    gmap çalışıyor

--- cSharp-offline-Gmap/Form1.cs
+++ cSharp-offline-Gmap/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
 
 namespace cSharp_offline_Gmap
 {
@@ -15,6 +17,19 @@ namespace cSharp_offline_Gmap
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Right;
+            gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+
+            gMapControl1.MapProvider = GMapProviders.GoogleSatelliteMap;
+            gMapControl1.Position = new PointLatLng(41.0133045, 29.0630608);
+
+            gMapControl1.MaxZoom = 100;
+            gMapControl1.MinZoom = 5;
+            gMapControl1.Zoom = 10;
         }
     }
 }
